3fb34adba801280a78edbf7d650ece1f80ed3ff9
zhangweiqun <zhangweiqun@3ekeji.cn>
2021-06-04 22:10:45 +0800

    feat: 新增加text文件

---
+++ src/views/text.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+   <div>
+       这是一个测试页面哈
+   </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
